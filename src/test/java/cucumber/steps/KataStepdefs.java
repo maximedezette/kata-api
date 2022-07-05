8561a284fb46f2dcaea8065829a16b9cf794a83a
maxime.dezette <m.dezette@gmail.com>
2022-07-05 22:38:41 +0200

    adding new assertion

--- src/test/java/cucumber/steps/KataStepdefs.java
+++ src/test/java/cucumber/steps/KataStepdefs.java
@@ -1,6 +1,12 @@
 package cucumber.steps;
 
+import com.aperotech.kata.domain.Kata;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -10,6 +16,8 @@ import io.restassured.specification.RequestSpecification;
 import net.minidev.json.JSONObject;
 import org.apache.http.HttpStatus;
 import org.springframework.boot.web.server.LocalServerPort;
+
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -21,10 +29,11 @@ public class KataStepdefs {
     private int port;
 
     @Before
-    public void setUp()  {
+    public void setUp() {
         RestAssured.baseURI = "http://localhost";
         RestAssured.port = port;
     }
+
     @When("A user asks for the Kata list")
     public void aUserAsksForTheKataList() {
         RequestSpecification request = RestAssured.given();
@@ -37,17 +46,35 @@ public class KataStepdefs {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.SC_OK);
     }
 
-    @Given("A user publish a new Kata")
-    public void aUserPublishANewKata() {
-            RequestSpecification request = RestAssured.given();
+    @Given("A user publish a new Kata named {word}")
+    public void aUserPublishANewKata(String name) {
+        RequestSpecification request = RestAssured.given();
 
 
         JSONObject requestParams = new JSONObject();
-        requestParams.put("name", "AperoTech");
+        requestParams.put("name", name);
 
         response = request
-                    .header("Content-Type", "application/json")
-                    .body(requestParams.toJSONString())
-                    .post("/kata");
+                .header("Content-Type", "application/json")
+                .body(requestParams.toJSONString())
+                .post("/kata");
+    }
+
+    @And("The following kata list should be in the response content")
+    public void theFollowingKataShouldBeInTheResponseContent(DataTable table) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        String serializedKata = response.getBody().asString();
+        List<Kata> actualKatas = mapper.readValue(serializedKata, new TypeReference<>() {
+        });
+
+        List<String> expectedKataNames = table.asList().subList(1, table.height());
+
+        for (String expectedKataName: expectedKataNames) {
+           boolean nameFound = actualKatas.stream()
+                    .anyMatch(kata -> kata.getName().equals(expectedKataName));
+           assertThat(nameFound).isTrue();
+        }
+
+
     }
 }
--- src/test/resources/features/kata.feature
+++ src/test/resources/features/kata.feature
@@ -1,6 +1,9 @@
 Feature: The kata can be retrieved
 
   Scenario: A user publish a new Kata
-    Given A user publish a new Kata
+    Given A user publish a new Kata named AperoTech
     When A user asks for the Kata list
     Then He should have a success response
+    And The following kata list should be in the response content
+          | name      |
+          | AperoTech |
